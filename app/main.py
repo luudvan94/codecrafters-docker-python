c4838e0e3ea2ab57c9488e74d54637875daaa97e
luudvan94 <33990794+luudvan94@users.noreply.github.com>
2024-01-02 14:54:57 -0500

    fetch image from registry

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from io import FileIO
 import subprocess
 import sys
 import stat
@@ -5,21 +6,56 @@ import os
 import tempfile
 import shutil
 import ctypes
+import urllib.request
+import json
+import tarfile
+
 UNSHARE = 272
 GET_PID = 39
 CLONE_NEWPID = 0x20000000
 libc = ctypes.CDLL(None)
 
+def get_token(image):
+    url = f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{image}:pull"
+    return json.loads(urllib.request.urlopen(url).read())["token"]
 
 def main():
+    image_info = sys.argv[2]
+    if ":" not in image_info:
+        image_info += ":latest"
+    image, version = image_info.split(":")
+    token = get_token(image)
+    manifest_url = (
+        f"https://registry.hub.docker.com/v2/library/{image}/manifests/{version}"
+    )
+    headers = {
+        "Accept": "application/vnd.docker.distribution.manifest.v2+json",
+        "Authorization": "Bearer " + token,
+    }
+    req = urllib.request.Request(manifest_url, headers=headers)
+    manifest = json.loads(urllib.request.urlopen(req).read())
     command = sys.argv[3]
     args = sys.argv[4:]
 
     with tempfile.TemporaryDirectory() as directory_path:
-        os.mkdir("{}/tmp".format(directory_path))
-        os.mkdir("{}/usr".format(directory_path))
-        os.mkdir("{}/usr/local".format(directory_path))
-        os.mkdir("{}/usr/local/bin".format(directory_path))
+        # os.mkdir("{}/tmp".format(directory_path))
+        # os.mkdir("{}/usr".format(directory_path))
+        # os.mkdir("{}/usr/local".format(directory_path))
+        # os.mkdir("{}/usr/local/bin".format(directory_path))
+        for layer in manifest["layers"]:
+            digest = layer["digest"]
+            layer_url = (
+                f"https://registry.hub.docker.com/v2/library/{image}/blobs/{digest}"
+            )
+            header = {"Authorization": "Bearer " + token}
+            req = urllib.request.Request(layer_url, headers=header)
+            response = urllib.request.urlopen(req)
+            tar_filename = f"/tmp/ff.tar.gz"
+            new_tar = open(tar_filename.format(digest), "wb")
+            new_tar.write(response.read())
+            tar = tarfile.open(tar_filename)
+            tar.extractall(directory_path)
+            tar.close()
         shutil.copyfile(
             "/usr/local/bin/docker-explorer",
             "{}/usr/local/bin/docker-explorer".format(directory_path),
